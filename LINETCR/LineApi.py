be4a35fb72887736d8277a3f7764f5a6508520de
Satria <37049360+Satria3@users.noreply.github.com>
2018-04-07 18:51:29 +0700

    Add files via upload

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -41,7 +41,7 @@ class LINE:
     else:
       raise Exception("invalid arguments")
 
-    self.authToken = self.Talk.authToken
+    self.authToken = self.Talk. authToken
     self.cert = self.Talk.cert
     self._headers = {
               'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 
@@ -460,14 +460,18 @@ class LINE:
       callback = def_callback
 
       prof = self.getProfile()
-
-      print("One Piece BOT")
+      
+      print("=========[ BOTTROX PROTECT]=========")
+      print("     Thanks for TCR and my friend")
+      print("=====================================")
+      print("BOTTROX PROTECT BOT")
       print("mid -> " + prof.mid)
       print("Nama -> " + prof.displayName)
       print("Token -> " + self.authToken)
       print("cert -> " + self.cert if self.cert is not None else "")
-
-
+      print("============[ S A T R I A ]==============")
+      print("   BOTTROX BOTS PROTECT.   ")
+      print("=====================================")
 
 class AniSearch(object):
   ## REQUIRED : urllib , requests
